220bf68416467835f8fbd35c2417c2f6d07c13c6
TJ <bearse.thomas@gmail.com>
2014-11-05 11:53:19 -0500

    framework for flashing on time up

--- pomodro.py
+++ pomodro.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
+import time
 import threading
 import sys
 
 #day, second, micro milli min hour week
 target = datetime.today()
-times = [timedelta(0, 10), timedelta(0, 5)]
+#times = [timedelta(0, 0, 0, 0, 20), timedelta(0, 0, 0, 0, 5)]
+times = [timedelta(0, 10)]
 time_index = 0
 
 def td_to_string(td):
@@ -13,13 +15,27 @@ def td_to_string(td):
     minutes, seconds = divmod(remainder, 60)
     return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
+def flash(sem):
+    while sem.acquire(False):
+        print "flash"
+        sem.release()
+        time.sleep(.5)
+        
+def alarm():
+    sem = threading.Semaphore(1)
+    t = threading.Thread(target=flash, args=(sem,) )
+    t.start()
+    #time.sleep(3)
+    #print '\rStarting timer for %s\n' % td_to_string(times[time_index])
+    raw_input()
+    sem.acquire()
+
 def updateTimer():
     global target
     global time_index
     global times
     if(target < datetime.today()):
-        print '\rStarting timer for %s' % td_to_string(times[time_index])
-        raw_input()
+        alarm()
         target = datetime.today() + times[time_index] 
         time_index += 1
         time_index %= len(times)
